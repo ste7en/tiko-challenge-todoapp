faa1102a5e602f1c6fe40c76d4bb21f64576a33c
Stefano Formicola <stefanoformicola@icloud.com>
2024-05-30 12:45:31 +0200

    chore: expo router configuration
    Remove the custom configuration for metro.config.js as it overrides the babelTransformerPath necessary to make Expo Router working

--- apps/todoapp/metro.config.js
+++ apps/todoapp/metro.config.js
@@ -1,9 +1,10 @@
 const { withNxMetro } = require('@nx/expo');
 const { getDefaultConfig } = require('@expo/metro-config');
-const { mergeConfig } = require('metro-config');
+//const { mergeConfig } = require('metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
+// const { assetExts, sourceExts } = defaultConfig.resolver;
+
 
 /**
  * Metro configuration
@@ -11,17 +12,17 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  *
  * @type {import('metro-config').MetroConfig}
  */
-const customConfig = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
-  },
-};
-
-module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
+// const customConfig = {
+//   transformer: {
+//     babelTransformerPath: require.resolve('react-native-svg-transformer'),
+//   },
+//   resolver: {
+//     assetExts: assetExts.filter((ext) => ext !== 'svg'),
+//     sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
+//   },
+// };
+// const nxConfig = withNxMetro(mergeConfig(defaultConfig, customConfig));
+module.exports = withNxMetro(defaultConfig, {
   // Change this to true to see debugging info.
   // Useful if you have issues resolving modules
   debug: false,
